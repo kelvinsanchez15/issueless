421dad52967f340cad6bb437d13da684a675c829
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-11-03 12:19:57 -0400

    set basic boilerplate with eslint, airbnb and prettier config

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Issueless</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>THIS IS MY HOME PAGE</div>
+    </>
+  );
+}
