ec88e89f85fb6223bfb69593ea13fb902d99ec15
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-11-04 11:07:25 -0400

    create custom MUI dark theme

--- src/components/styles/theme.js
+++ src/components/styles/theme.js
@@ -1,12 +1,30 @@
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
 
 // Create a theme instance with responsive font sizes
 const theme = responsiveFontSizes(
   createMuiTheme({
     palette: {
+      type: 'dark',
+      primary: {
+        main: '#BB86FC',
+      },
+      secondary: {
+        main: '#03DAC5',
+      },
+
       error: {
-        main: red.A400,
+        main: '#CF6679',
+      },
+      background: {
+        paper: '#1E1E1E',
+        default: '#121212',
+      },
+    },
+    overrides: {
+      MuiAppBar: {
+        colorDefault: {
+          backgroundColor: '#1F1F1F',
+        },
       },
     },
   })
