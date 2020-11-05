1d0bc4019c991c66dee36f897407ee7968db6c1a
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-11-05 19:34:30 -0400

    create withSession hoc

---
+++ src/utils/withSession.js
@@ -0,0 +1,18 @@
+import { getSession } from 'next-auth/client';
+
+const withSession = (handler) => {
+  return async (req, res) => {
+    try {
+      const session = await getSession({ req });
+      if (!session) {
+        throw new Error();
+      }
+      req.session = session;
+      return handler(req, res);
+    } catch (error) {
+      return res.status(401).json({ error: 'Please authenticate' });
+    }
+  };
+};
+
+export default withSession;
