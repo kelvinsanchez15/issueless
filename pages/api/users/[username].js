a45f92d19ebac52fd1ca9d93dd61e940f44f4d02
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-11-06 21:18:47 -0400

    create public user endpoint

---
+++ pages/api/users/[username].js
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({
+      message: `The HTTP ${req.method} method is not supported at this route.`,
+    });
+  }
+  const { username } = req.query;
+  try {
+    const user = await prisma.user.findOne({ where: { username } });
+    if (!user) return res.status(404).json({ message: 'Not found' });
+    return res.json(user);
+  } catch (error) {
+    return res.status(400).json({ message: 'Something went wrong' });
+  }
+}
