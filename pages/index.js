0f0dad8ccc682c1d6a35c6a5841aaff628f47aaa
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-11-11 17:53:06 -0400

    delete old test issues page and create placeholder homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,47 @@
 import Head from 'next/head';
 import Link from 'src/components/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Container,
+  Paper,
+  List,
+  Divider,
+  ListItem,
+  ListItemText,
+  Typography,
+  ListItemIcon,
+  Avatar,
+} from '@material-ui/core';
+import { PrismaClient } from '@prisma/client';
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(4),
+  },
+  avatar: {
+    width: 'auto',
+    height: 'auto',
+  },
+  tempWrapper: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+}));
+
+export async function getServerSideProps() {
+  const users = await prisma.user.findMany({
+    select: { id: true, name: true, username: true, bio: true, image: true },
+  });
+  if (!users) return { notFound: true };
+  return {
+    props: { users },
+  };
+}
+
+export default function Home({ users }) {
+  const classes = useStyles();
   return (
     <>
       <Head>
@@ -9,8 +49,34 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>THIS IS MY HOME PAGE</div>
-      <Link href="/test-user/test-project/issues">ISSUES</Link>
+      <Container>
+        <div>PLACE HOLDER HOME PAGE</div>
+        <Paper className={classes.root}>
+          <List disablePadding>
+            <Divider />
+            {users.map((user) => (
+              <ListItem key={user.id} divider>
+                <ListItemIcon>
+                  <Avatar src={user.image} />
+                </ListItemIcon>
+                <ListItemText disableTypography>
+                  <Typography variant="h6">
+                    <Link href={`/${user.username}`}>{user.name}</Link>
+                  </Typography>
+                  <Typography
+                    variant="subtitle2"
+                    color="textSecondary"
+                    gutterBottom
+                  >
+                    {user.username}
+                  </Typography>
+                  <Typography variant="body1">{user.bio}</Typography>
+                </ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Paper>
+      </Container>
     </>
   );
 }
