d79926c0688ab3bc8fb1090dccdb593c09cd656e
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-11-17 18:03:44 -0400

    fill welcome page with form to create username

--- pages/welcome.js
+++ pages/welcome.js
@@ -1,16 +1,141 @@
+import React from 'react';
 import Head from 'next/head';
-import Link from 'src/components/Link';
+import { useRouter } from 'next/router';
+import { Formik, Form, Field } from 'formik';
+import { TextField } from 'formik-material-ui';
+import * as Yup from 'yup';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Container,
+  Typography,
+  Button,
+  Avatar,
+  Snackbar,
+  CircularProgress,
+} from '@material-ui/core';
+import { LockOutlined as LockOutlinedIcon } from '@material-ui/icons';
+import Alert from '@material-ui/lab/Alert';
 
-export default function Welcome() {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submitWrapper: {
+    margin: theme.spacing(1, 0, 2),
+    position: 'relative',
+  },
+  buttonProgress: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  },
+}));
+
+export default function LoginPage() {
+  const classes = useStyles();
+  const router = useRouter();
+  const [errorAlert, setErrorAlert] = React.useState(false);
+
   return (
     <>
       <Head>
         <title>Welcome</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>Here is going to be the form for new user extra info</div>
-      <Link href="/test-user/test-project/issues">ISSUES</Link>
+      <Container maxWidth="xs" component="main">
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+
+          <Typography component="h1" variant="h5">
+            One more step
+          </Typography>
+          <Typography component="h1" variant="h5">
+            Create a username
+          </Typography>
+
+          <Formik
+            initialValues={{ username: '' }}
+            validationSchema={Yup.object({
+              username: Yup.string().trim().strict(true).required('Required'),
+            })}
+            onSubmit={async (values, { setSubmitting, resetForm }) => {
+              setErrorAlert(false);
+              try {
+                const { username } = values;
+                const res = await fetch('/api/user', {
+                  method: 'PATCH',
+                  headers: { 'Content-Type': 'application/json' },
+                  body: JSON.stringify({ username }),
+                });
+                if (!res.ok) {
+                  setSubmitting(false);
+                  const { error } = await res.json();
+                  throw new Error(error);
+                }
+                router.push(`/${username}`);
+                resetForm();
+                setSubmitting(false);
+              } catch (error) {
+                setErrorAlert(true);
+              }
+            }}
+          >
+            {({ handleSubmit, isSubmitting }) => (
+              <Form onSubmit={handleSubmit} className={classes.form}>
+                <Field
+                  component={TextField}
+                  fullWidth
+                  variant="outlined"
+                  name="username"
+                  type="username"
+                  label="Username *"
+                  autoComplete="username"
+                  helperText=" "
+                />
+
+                <div className={classes.submitWrapper}>
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Submit
+                  </Button>
+                  {isSubmitting && (
+                    <CircularProgress
+                      size={24}
+                      className={classes.buttonProgress}
+                    />
+                  )}
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+
+        <Snackbar open={errorAlert}>
+          <Alert severity="error">
+            Username has already been used. Try again!
+          </Alert>
+        </Snackbar>
+      </Container>
     </>
   );
 }
