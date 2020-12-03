fde8b095f34fba3b1589e33dcedb35ac0a2f7a78
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-12-03 11:27:33 -0400

    update endpoint logic and add role permisions

--- pages/api/repos/[owner]/[repo]/issues/index.js
+++ pages/api/repos/[owner]/[repo]/issues/index.js
@@ -10,30 +10,53 @@ const handler = async (req, res) => {
     });
     return;
   }
+
+  const { owner: repoOwner, repo: repoName } = req.query;
+  const { username: loggedUser } = req.session;
+  const isRepoOwner = loggedUser === repoOwner;
+
+  const user = await prisma.user.findOne({
+    where: { username: repoOwner },
+    select: { id: true },
+  });
+  if (!user) {
+    res.status(404).json({ message: 'Not found' });
+    return;
+  }
+  const repository = await prisma.repository.findOne({
+    where: {
+      repositories_name_owner_id_key: {
+        name: repoName,
+        ownerId: user.id,
+      },
+    },
+    select: { id: true },
+  });
+  if (!repository) {
+    res.status(404).json({ message: 'Not found' });
+    return;
+  }
+
   try {
-    const { owner, repo: repoName } = req.query;
-    const userId = Number(req.session.userId);
-    const { title, body, labels } = req.body;
+    const { title, body, assignee, labels } = req.body;
     if (!title) {
       res.status(403).json({ message: '[title] field is required' });
       return;
     }
-    const { id: ownerId } = await prisma.user.findOne({
-      where: { username: owner },
-      select: { id: true },
-    });
     const data = {
       title,
       ...(body && { body }),
-      ...(labels && { labels: { connect: labels } }),
-      user: { connect: { id: userId } },
-      repositories: {
-        connect: {
-          repositories_name_owner_id_key: { name: repoName, ownerId },
-        },
-      },
+      // Allow only the repo owner to add "assignee" and "labels" fields
+      ...(assignee && isRepoOwner && { assignee }),
+      ...(labels && isRepoOwner && { labels: { connect: labels } }),
     };
-    const issue = await prisma.issue.create({ data });
+    const issue = await prisma.issue.create({
+      data: {
+        ...data,
+        user: { connect: { username: loggedUser } },
+        repositories: { connect: { id: repository.id } },
+      },
+    });
     res.status(201).json(issue);
   } catch (error) {
     res.status(400).json({ message: 'Something went wrong' });
