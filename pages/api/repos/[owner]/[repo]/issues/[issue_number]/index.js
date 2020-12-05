fe623d12a7215a601ebcd3720468cc74bbe79742
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-12-05 12:33:01 -0400

    handle get request and make use of db util functions

--- pages/api/repos/[owner]/[repo]/issues/[issue_number]/index.js
+++ pages/api/repos/[owner]/[repo]/issues/[issue_number]/index.js
@@ -1,56 +1,37 @@
-import withSession from 'src/utils/withSession';
+import { getSession } from 'next-auth/client';
 import { PrismaClient } from '@prisma/client';
+import getIssueAndComments from 'src/utils/db/getIssueAndComments';
+import getIssueByQuery from 'src/utils/db/getIssueByQuery';
 
 const prisma = new PrismaClient();
 
-const handler = async (req, res) => {
-  const { username: loggedUser } = req.session;
-  const {
-    owner: repoOwner,
-    repo: repoName,
-    issue_number: issueNumber,
-  } = req.query;
-
-  const user = await prisma.user.findOne({
-    where: { username: repoOwner },
-    select: { id: true },
-  });
-  if (!user) {
-    res.status(404).json({ message: 'Not found' });
-    return;
-  }
-  const repository = await prisma.repository.findOne({
-    where: {
-      repositories_name_owner_id_key: {
-        name: repoName,
-        ownerId: user.id,
-      },
-    },
-    select: { id: true },
-  });
-  if (!repository) {
-    res.status(404).json({ message: 'Not found' });
-    return;
-  }
-  const issue = await prisma.issue.findOne({
-    where: {
-      Issue_number_repo_id_key: {
-        number: Number(issueNumber),
-        repoId: repository.id,
-      },
-    },
-    select: { id: true, user: { select: { username: true } } },
-  });
-  if (!issue) {
-    res.status(404).json({ message: 'Not found' });
-    return;
-  }
-  const isIssueOwner = loggedUser === issue.user.username;
-  const isRepoOwner = loggedUser === repoOwner;
-
+export default async function handler(req, res) {
+  const { query } = req;
   switch (req.method) {
+    case 'GET':
+      try {
+        const issueAndComments = await getIssueAndComments(query, prisma);
+        res.json(issueAndComments);
+      } catch (error) {
+        if (error.message === 'Not found') {
+          res.status(404).json({ message: error.message });
+          return;
+        }
+        res.status(400).json({ message: 'Something went wrong' });
+      }
+      break;
     case 'PATCH':
       try {
+        const session = await getSession({ req });
+        if (!session) {
+          res.status(401).json({ message: 'Please authenticate' });
+          return;
+        }
+        const loggedUser = session.username;
+        const { owner: repoOwner } = req.query;
+        const issue = await getIssueByQuery(query, prisma);
+        const isIssueOwner = loggedUser === issue.user.username;
+        const isRepoOwner = loggedUser === repoOwner;
         // Allow only the issue owner or repo owner permission to update issues
         if (!(isIssueOwner || isRepoOwner)) {
           res.status(403).json({ message: 'Forbidden' });
@@ -72,8 +53,8 @@ const handler = async (req, res) => {
         });
         res.json(updatedIssue);
       } catch (error) {
-        if (error.code === 'P2016') {
-          res.status(404).json({ message: 'Not found' });
+        if (error.message === 'Not found') {
+          res.status(404).json({ message: error.message });
           return;
         }
         res.status(400).json({ message: 'Something went wrong' });
@@ -81,17 +62,25 @@ const handler = async (req, res) => {
       break;
     case 'DELETE':
       try {
+        const session = await getSession({ req });
+        if (!session) {
+          res.status(401).json({ message: 'Please authenticate' });
+          return;
+        }
+        const loggedUser = session.username;
+        const { owner: repoOwner } = req.query;
         // Allow only the repo owner permission to delete issues
-        if (!isRepoOwner) {
+        if (loggedUser !== repoOwner) {
           res.status(403).json({ message: 'Forbidden' });
           return;
         }
+        const issue = await getIssueByQuery(query, prisma);
         // Delete issue: /api/{owner}]/{repo}/issues/{issue_number}
         await prisma.issue.delete({ where: { id: issue.id } });
         res.status(204).end();
       } catch (error) {
-        if (error.code === 'P2016') {
-          res.status(404).json({ message: 'Not found' });
+        if (error.message === 'Not found') {
+          res.status(404).json({ message: error.message });
           return;
         }
         res.status(400).json({ message: 'Something went wrong' });
@@ -103,6 +92,4 @@ const handler = async (req, res) => {
       });
       break;
   }
-};
-
-export default withSession(handler);
+}
