9ca77091cf46bc8d1105a4cadb4c8cb483154673
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-12-05 12:30:44 -0400

    create getIssueAndComments db util function

---
+++ src/utils/db/getIssueAndComments.js
@@ -0,0 +1,43 @@
+const getIssueAndComments = async (query, prisma) => {
+  const { owner, repo: repoName, issue_number: issueNumber } = query;
+  const user = await prisma.user.findOne({
+    where: { username: owner },
+    select: { id: true },
+  });
+  if (!user) {
+    throw new Error('Not found');
+  }
+  const repository = await prisma.repository.findOne({
+    where: {
+      repositories_name_owner_id_key: {
+        name: repoName,
+        ownerId: user.id,
+      },
+    },
+    select: { id: true },
+  });
+  if (!repository) {
+    throw new Error('Not found');
+  }
+  const issue = await prisma.issue.findOne({
+    where: {
+      Issue_number_repo_id_key: {
+        number: Number(issueNumber),
+        repoId: repository.id,
+      },
+    },
+    include: {
+      user: { select: { username: true, image: true } },
+      comments: {
+        include: { user: { select: { username: true, image: true } } },
+      },
+      labels: true,
+    },
+  });
+  if (!issue) {
+    throw new Error('Not found');
+  }
+  return issue;
+};
+
+export default getIssueAndComments;
