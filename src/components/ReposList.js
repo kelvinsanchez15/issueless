20f27023217a2c83b1049dd8a91a4b401d7c72b7
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-12-08 12:23:47 -0400

    add fallback header when user repo list is empty

--- src/components/ReposList.js
+++ src/components/ReposList.js
@@ -14,6 +14,10 @@ const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: theme.spacing(2),
   },
+  fallbackHeader: {
+    padding: theme.spacing(4),
+    marginTop: theme.spacing(4),
+  },
 }));
 
 export default function ReposList({ repos, username }) {
@@ -22,23 +26,29 @@ export default function ReposList({ repos, username }) {
     <Paper className={classes.root}>
       <List disablePadding>
         <Divider />
-        {repos.map((repo) => (
-          <ListItem key={repo.id} divider>
-            <ListItemText disableTypography>
-              <Typography variant="h6">
-                <Link href={`/${username}/${repo.name}/issues`}>
-                  {repo.name}
-                </Link>
-              </Typography>
-              <Typography variant="body1" gutterBottom>
-                {repo.description}
-              </Typography>
-              <Typography variant="subtitle2" color="textSecondary">
-                {`Created ${formatDate(repo.createdAt)}`}
-              </Typography>
-            </ListItemText>
-          </ListItem>
-        ))}
+        {repos.length > 0 ? (
+          repos.map((repo) => (
+            <ListItem key={repo.id} divider>
+              <ListItemText disableTypography>
+                <Typography variant="h6">
+                  <Link href={`/${username}/${repo.name}/issues`}>
+                    {repo.name}
+                  </Link>
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  {repo.description}
+                </Typography>
+                <Typography variant="subtitle2" color="textSecondary">
+                  {`Created ${formatDate(repo.createdAt)}`}
+                </Typography>
+              </ListItemText>
+            </ListItem>
+          ))
+        ) : (
+          <Typography className={classes.fallbackHeader} variant="h6">
+            {`${username} doesn’t have any public repositories yet.`}
+          </Typography>
+        )}
       </List>
     </Paper>
   );
