bfafc983ff027f6d7393930fcc14f551f8e9fd83
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-12-10 12:12:31 -0400

    fix util function

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -1,3 +1,4 @@
-const slugify = (str) => str.replace(/[^\w ]+/g, '').replace(/\s+/g, '-');
+const slugify = (str) =>
+  str.replace(/[^a-zA-Z0-9_ -]+/g, '').replace(/\s+/g, '-');
 
 export default slugify;
