e262893b14e085f19f9d970ef3fbb9875454526c
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-12-13 12:38:19 -0400

    create hero for home page

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,15 @@
 import Head from 'next/head';
-import Link from 'src/components/Link';
 import { makeStyles } from '@material-ui/core/styles';
-import {
-  Container,
-  Paper,
-  List,
-  Divider,
-  ListItem,
-  ListItemText,
-  Typography,
-  ListItemIcon,
-  Avatar,
-} from '@material-ui/core';
-import { PrismaClient } from '@prisma/client';
+import { Container } from '@material-ui/core';
+import Hero from 'src/components/home/Hero';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: theme.spacing(4),
   },
-  avatar: {
-    width: 'auto',
-    height: 'auto',
-  },
-  tempWrapper: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
 }));
 
-const prisma = new PrismaClient();
-
-export async function getStaticProps() {
-  try {
-    const users = await prisma.user.findMany({
-      select: { id: true, name: true, username: true, bio: true, image: true },
-    });
-    if (!users) return { notFound: true };
-    return {
-      props: { users },
-      revalidate: 2,
-    };
-  } finally {
-    await prisma.$disconnect();
-  }
-}
-
-export default function Home({ users }) {
+export default function Home() {
   const classes = useStyles();
   return (
     <>
@@ -55,31 +19,7 @@ export default function Home({ users }) {
       </Head>
 
       <Container>
-        <Paper className={classes.root}>
-          <List disablePadding>
-            <Divider />
-            {users.map((user) => (
-              <ListItem key={user.id} divider>
-                <ListItemIcon>
-                  <Avatar src={user.image} />
-                </ListItemIcon>
-                <ListItemText disableTypography>
-                  <Typography variant="h6">
-                    <Link href={`/${user.username}`}>{user.name}</Link>
-                  </Typography>
-                  <Typography
-                    variant="subtitle2"
-                    color="textSecondary"
-                    gutterBottom
-                  >
-                    {user.username}
-                  </Typography>
-                  <Typography variant="body1">{user.bio}</Typography>
-                </ListItemText>
-              </ListItem>
-            ))}
-          </List>
-        </Paper>
+        <Hero />
       </Container>
     </>
   );
---
+++ src/components/home/Hero.js
@@ -0,0 +1,86 @@
+import Image from 'next/image';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Container,
+  Button,
+  Typography,
+  Grid,
+  Hidden,
+} from '@material-ui/core/';
+import Link from 'src/components/Link';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: `calc(100vh - ${theme.mixins.toolbar.minHeight}px)`,
+    display: 'flex',
+    alignItems: 'center',
+    [theme.breakpoints.up('sm')]: {
+      minHeight: `calc(100vh - 64px)`,
+    },
+  },
+  image: {
+    borderRadius: theme.shape.borderRadius,
+    boxShadow: theme.shadows[4],
+  },
+  mb8: {
+    marginBottom: theme.spacing(8),
+  },
+  mr2: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+export default function Hero() {
+  const classes = useStyles();
+  return (
+    <section id="home" className={classes.root}>
+      <Container>
+        <Grid container justify="space-between">
+          <Grid item xs={12} md={6}>
+            <div className={classes.mb8}>
+              <Typography component="h1" variant="h2" gutterBottom>
+                Issueless
+              </Typography>
+
+              <Typography
+                component="p"
+                variant="subtitle1"
+                color="textSecondary"
+              >
+                JAMStack Issue Tracker inspired on GitHub issues.
+              </Typography>
+            </div>
+            <div>
+              <Button
+                component={Link}
+                variant="contained"
+                color="primary"
+                href="/signin"
+                naked
+                className={classes.mr2}
+              >
+                Sign in
+              </Button>
+
+              <Button href="#features" variant="outlined" color="primary">
+                Check features
+              </Button>
+            </div>
+          </Grid>
+          <Hidden smDown>
+            <Grid item md={6}>
+              <Image
+                src="/images/headerImage.jpg"
+                className={classes.image}
+                alt="header example"
+                width={1024}
+                height={566}
+                layout="responsive"
+              />
+            </Grid>
+          </Hidden>
+        </Grid>
+      </Container>
+    </section>
+  );
+}
