44f102be5a822fb570d66a331a20944fde5834a2
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-12-19 13:53:39 -0400

    refactor arrow function

--- src/utils/withSession.js
+++ src/utils/withSession.js
@@ -1,18 +1,16 @@
 import { getSession } from 'next-auth/client';
 
-const withSession = (handler) => {
-  return async (req, res) => {
-    try {
-      const session = await getSession({ req });
-      if (!session) {
-        throw new Error();
-      }
-      req.session = session;
-      return handler(req, res);
-    } catch (error) {
-      return res.status(401).json({ message: 'Please authenticate' });
+const withSession = (handler) => async (req, res) => {
+  try {
+    const session = await getSession({ req });
+    if (!session) {
+      throw new Error();
     }
-  };
+    req.session = session;
+    return handler(req, res);
+  } catch (error) {
+    return res.status(401).json({ message: 'Please authenticate' });
+  }
 };
 
 export default withSession;
