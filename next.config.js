b6086f62ae808af12763b0da51ea1a4830f291a2
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2020-12-30 10:41:04 -0400

    add extense domains list to support different image hosting

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 module.exports = {
   images: {
     domains: [
+      'avatars0.githubusercontent.com',
+      'avatars1.githubusercontent.com',
+      'avatars2.githubusercontent.com',
       'avatars3.githubusercontent.com',
+      'avatars4.githubusercontent.com',
+      'avatars5.githubusercontent.com',
+      'avatars6.githubusercontent.com',
+      'avatars7.githubusercontent.com',
+      'storage.googleapis.com',
+      'lh1.googleusercontent.com',
+      'lh2.googleusercontent.com',
       'lh3.googleusercontent.com',
+      'lh4.googleusercontent.com',
+      'lh5.googleusercontent.com',
+      'lh6.googleusercontent.com',
       'robohash.org',
     ],
   },
